f75c5f25d3a6d3c73b89ca724ba897c81da93cea
beryah <sgm4320@gmail.com>
2014-06-17 16:14:28 +0800

    Model in model test.

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Telerik.Charting;
 using Telerik.UI.Xaml.Controls.Chart;
@@ -67,6 +68,66 @@ namespace App1
                 }
             }
         }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get
+            {
+                return "Hello!!";
+            }
+        }
+
+        public Model Model
+        {
+            get
+            {
+                return new Model() { ModelText = "YYYYY" };
+            }
+        }
+
+        public List<Model> Models
+        {
+            get
+            {
+                return new List<Model>()
+                {
+                    new Model(){ModelText="Model_1", ModelA = new ModelInModel(){TextA="1AA", TextB="1AB"}, ModelB = new ModelInModel(){TextA="1BA", TextB="1BB"}, ModelC = new ModelInModel(){TextA="1CA", TextB="1CB"}},
+                    new Model(){ModelText="Model_2", ModelA = new ModelInModel(){TextA="2AA", TextB="2AB"}, ModelB = new ModelInModel(){TextA="2BA", TextB="2BB"}, ModelC = new ModelInModel(){TextA="3CA", TextB="3CB"}},
+                    new Model(){ModelText="Model_3", ModelA = new ModelInModel(){TextA="3AA", TextB="3AB"}, ModelB = new ModelInModel(){TextA="2BA", TextB="2BB"}, ModelC = new ModelInModel(){TextA="3CA", TextB="3CB"}},
+                };
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class Model
+    {
+        public string ModelText { get; set; }
+
+        public ModelInModel ModelA { get; set; }
+
+        public ModelInModel ModelB { get; set; }
+
+        public ModelInModel ModelC { get; set; }
+    }
+
+    public class ModelInModel
+    {
+        public string TextA { get; set; }
+
+        public string TextB { get; set; }
     }
 
     public class PieData
