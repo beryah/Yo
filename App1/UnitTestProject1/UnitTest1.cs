fc108dc749304b0f29f1f07d0f371b88ebb263f9
beryah <sgm4320@gmail.com>
2014-07-28 17:59:04 +0800

    Calculate Chinese lunar date

---
+++ App1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = new DateTime(1912, 2, 5).GetChineseLunarDate();
+            var b = new DateTime(1912, 2, 24).GetChineseLunarDate();
+            var c = new DateTime(1912, 2, 25).GetChineseLunarDate();
+            var d = new DateTime(1912, 2, 26).GetChineseLunarDate();
+            
+            int spsp = 0;
+            spsp++;
+        }
+    }
+}
