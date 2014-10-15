e367813c7151607acfb62e7818ce74ce5d231aef
beryah <sgm4320@gmail.com>
2014-10-15 10:52:35 +0800

    Test

--- PrintTest/PrintTest/MainPage.xaml.cs
+++ PrintTest/PrintTest/MainPage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Windows.Graphics.Printing;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Printing;
 
 // 空白頁項目範本已記錄在 http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -14,123 +10,16 @@ namespace PrintTest
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private PrintDocument printDocument = null;
-        private IPrintDocumentSource printDocumentSource = null;
-        private List<UIElement> printPreviewPages = null;
-        private event EventHandler pagesCreated;
-
         public MainPage()
         {
             this.InitializeComponent();
-            printPreviewPages = new List<UIElement>();
-            RegisterForPrinting();
         }
 
-        private async void Print_Click(object sender, RoutedEventArgs e)
+        private Printer printer = new Printer();
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            await Windows.Graphics.Printing.PrintManager.ShowPrintUIAsync();
-        }
-
-        private void RegisterForPrinting()
-        {
-            // Create the PrintDocument.
-            printDocument = new PrintDocument();
-
-            // Save the DocumentSource.
-            printDocumentSource = printDocument.DocumentSource;
-
-            // Add an event handler which creates preview pages.
-            printDocument.Paginate += CreatePrintPreviewPages;
-
-            // Add an event handler which provides a specified preview page.
-            printDocument.GetPreviewPage += GetPrintPreviewPage;
-
-            // Add an event handler which provides all final print pages.
-            printDocument.AddPages += AddPrintPages;
-
-            // Create a PrintManager and add a handler for printing initialization.
-            PrintManager printMan = PrintManager.GetForCurrentView();
-            printMan.PrintTaskRequested += PrintTaskRequested;
-
-            // Initialize print content for this scenario
-            PreparePrintContent();
-        }
-
-        private void CreatePrintPreviewPages(object sender, PaginateEventArgs e)
-        {
-            // Clear the cache of preview pages 
-            printPreviewPages.Clear();
-
-            var printPage = new PrintPage();
-
-            printPreviewPages.Add(printPage);
-
-            // Get the PrintTaskOptions
-            PrintTaskOptions printingOptions = ((PrintTaskOptions)e.PrintTaskOptions);
-
-            // Get the page description to deterimine how big the page is
-            PrintPageDescription pageDescription = printingOptions.GetPageDescription(0);
-
-            var w = pageDescription.PageSize.Width;
-            var h = pageDescription.PageSize.Height;
-
-            if (pagesCreated != null)
-            {
-                pagesCreated.Invoke(printPreviewPages, null);
-            }
-
-            PrintDocument printDoc = (PrintDocument)sender;
-
-            // Report the number of preview pages created
-            printDoc.SetPreviewPageCount(printPreviewPages.Count, PreviewPageCountType.Intermediate);
-        }
-
-        private void GetPrintPreviewPage(object sender, GetPreviewPageEventArgs e)
-        {
-            PrintDocument printDoc = (PrintDocument)sender;
-
-            printDoc.SetPreviewPage(e.PageNumber, printPreviewPages[e.PageNumber - 1]);
-        }
-
-        private void AddPrintPages(object sender, AddPagesEventArgs e)
-        {
-            // Loop over all of the preview pages and add each one to  add each page to be printied
-            for (int i = 0; i < printPreviewPages.Count; i++)
-            {
-                // We should have all pages ready at this point...
-                printDocument.AddPage(printPreviewPages[i]);
-            }
-
-            PrintDocument printDoc = (PrintDocument)sender;
-
-            // Indicate that all of the print pages have been provided
-            printDoc.AddPagesComplete();
-        }
-
-        private void PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs e)
-        {
-            PrintTask printTask = null;
-            printTask = e.Request.CreatePrintTask("C# Printing SDK Sample", sourceRequested =>
-            {
-                // Print Task event handler is invoked when the print job is completed.
-                printTask.Completed += async (s, args) =>
-                {
-                    // Notify the user when the print operation fails.
-                    if (args.Completion == PrintTaskCompletion.Failed)
-                    {
-                        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-                        {
-                            Status.Text = "Failed to print.";
-                        });
-                    }
-                };
-
-                sourceRequested.SetSource(printDocumentSource);
-            });
-        }
-
-        private void PreparePrintContent()
-        {
+            await printer.Print();
         }
     }
 }
---
+++ PrintTest/PrintTest/Printer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Graphics.Printing;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Printing;
+
+namespace PrintTest
+{
+    public class Printer
+    {
+        #region Constructor & Properties
+
+        public Printer()
+        {
+            this.PrintDocument = new PrintDocument();
+            this.PrintDocumentSource = this.PrintDocument.DocumentSource;
+            this.PrintManager = PrintManager.GetForCurrentView();
+            this.PrintPreviewPages = new List<UIElement>();
+
+            this.PrintDocument.Paginate += this.CreatePrintPreviewPages;
+            this.PrintDocument.GetPreviewPage += this.GetPrintPreviewPage;
+            this.PrintDocument.AddPages += this.AddPrintPages;
+        }
+
+        private PrintDocument PrintDocument { get; set; }
+
+        private IPrintDocumentSource PrintDocumentSource { get; set; }
+
+        private PrintManager PrintManager { get; set; }
+
+        private List<UIElement> PrintPreviewPages { get; set; }
+
+        #endregion Constructor & Properties
+
+        public async Task Print()
+        {
+            this.RegisterForPrinting();
+            await PrintManager.ShowPrintUIAsync();
+            this.UnregisterForPrinting();
+        }
+
+        private List<UIElement> GetPreviewPages()
+        {
+            return new List<UIElement>()
+            {
+                new PrintPage(),
+                new PrintPage(),
+                new PrintPage(),
+                new PrintPage(),
+            };
+        }
+
+        #region Private Methods
+
+        private void RegisterForPrinting()
+        {
+            this.PrintManager.PrintTaskRequested += this.PrintTaskRequested;
+        }
+
+        private void UnregisterForPrinting()
+        {
+            this.PrintManager.PrintTaskRequested -= this.PrintTaskRequested;
+        }
+
+        private void CreatePrintPreviewPages(object sender, PaginateEventArgs e)
+        {
+            PrintTaskOptions printingOptions = ((PrintTaskOptions)e.PrintTaskOptions);
+
+            PrintPageDescription pageDescription = printingOptions.GetPageDescription(0);
+
+            this.PrintPreviewPages = this.GetPreviewPages();
+
+            PrintDocument printDoc = (PrintDocument)sender;
+
+            printDoc.SetPreviewPageCount(this.PrintPreviewPages.Count, PreviewPageCountType.Intermediate);
+        }
+
+        private void GetPrintPreviewPage(object sender, GetPreviewPageEventArgs e)
+        {
+            PrintDocument printDoc = (PrintDocument)sender;
+            printDoc.SetPreviewPage(e.PageNumber, this.PrintPreviewPages[e.PageNumber - 1]);
+        }
+
+        private void AddPrintPages(object sender, AddPagesEventArgs e)
+        {
+            for (int i = 0; i < this.PrintPreviewPages.Count; i++)
+            {
+                this.PrintDocument.AddPage(this.PrintPreviewPages[i]);
+            }
+
+            PrintDocument printDoc = (PrintDocument)sender;
+            printDoc.AddPagesComplete();
+        }
+
+        private void PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs e)
+        {
+            PrintTask printTask = null;
+            printTask = e.Request.CreatePrintTask("Print Test Task", sourceRequested =>
+            {
+                sourceRequested.SetSource(this.PrintDocumentSource);
+            });
+        }
+
+        #endregion Private Methods
+    }
+}
